5c4b93101421adeb70c73769d2ed11171e060959
Caio Carneiro <caiozinx@gmail.com>
2019-08-07 03:06:38 -0300

    Finalizado backend x)

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,11 +1,47 @@
 const axios = require('axios');
+const Dev = require('../models/Dev')
 
 module.exports = {
+    async index(req, res){
+        const { user } = req.headers;
+
+        const loggedDev = await Dev.findById(user);
+
+//$and === Condicional && para todas as verificações dentro do array.
+//$ne === Not Equal.
+//$nin === Not In.
+
+        const users = await Dev.find({
+            $and: [
+                { _id: { $ne: user } },
+                { _id: { $nin: loggedDev.likes } },
+                { _id: { $nin: loggedDev.dislikes } },            
+            ],
+        })
+
+        return res.json(users);
+    },
+
     async store(req, res){
         const { username } = req.body;
 
-        const response = axios.get('');
+        const userExists = await Dev.findOne({ user: username });
+
+        if(userExists){
+            return res.json(userExists);
+        }
+
+        const response = await axios.get(`https://api.github.com/users/${username}`);
         
-        return await res.json({ ok: true });
+        const { name, bio, avatar_url: avatar } = response.data;
+
+        const dev = await Dev.create({
+            name,
+            user: username,
+            bio,
+            avatar
+        })
+
+        return  res.json(dev);
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikesController = require('./controllers/LikeController');
+const DislikesController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
@@ -7,6 +9,9 @@ routes.get('/', (req, res) => {
     return res.json({ message: `Hello ${req.query.name}` });
 });
 
-routes.post('/devs', DevController.store)
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikesController.store);
+routes.post('/devs/:devId/dislikes', DislikesController.store);
 
 module.exports = routes;
